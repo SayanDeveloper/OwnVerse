b8ec31ac9e2e3fba04a5a917a0baf8a204bfcb7a
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-01 03:48:29 +0530

    created navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, Flex, Text, Button, useDisclosure } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { motion } from "framer-motion";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+const variants = {
+  open: { opacity: 1, height: "auto" },
+  closed: { opacity: 0, height: 0 }
+};
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box bg="#131415" py={3} px={{ base: 4, md: 20 }}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <Box>
+          <Text fontSize="3xl" fontWeight="bold" color="white">Logo</Text>
+        </Box>
+        <Box display={{ base: "block", md: "none" }} onClick={isOpen ? onClose : onOpen}>
+          {isOpen ? <CloseIcon color="white" w={6} h={6} /> : <HamburgerIcon color="white" w={6} h={6} />}
+        </Box>
+        <Box display={{ base: "none", md: "block" }}>
+          <Flex alignItems="center">
+            <Button  variant="ghost" mx={2} _hover={{ color: "gray.500" }} color="white">Home</Button>
+            <Button variant="ghost" mx={2} _hover={{ color: "gray.500" }} color="white">About</Button>
+            <Button variant="ghost" mx={2} _hover={{ color: "gray.500" }} color="white">Contact</Button>
+            <ConnectButton/>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,51 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, mainnet, WagmiConfig } from "wagmi";
+import { arbitrum, bscTestnet, optimism, polygon, polygonMumbai } from "wagmi/chains";
+import { useState, useEffect } from "react";
+import Navbar from "@/components/Navbar";
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, bscTestnet, polygonMumbai, optimism, arbitrum],
+  [
+    alchemyProvider({ apiKey: "IFTdS-HwrT3eufcbPA5nvfm9iVfnMq7j" }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "OwnVerse",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App({ Component, pageProps }) {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    setReady(true);
+  });
+  return (
+    <ChakraProvider>
+      {ready ? (
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider coolMode chains={chains} theme={darkTheme()}>
+              <Navbar />
+              <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+      ) : null}
+    </ChakraProvider>
+  );
+};
