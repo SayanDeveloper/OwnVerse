4c46275f70efeb3e2506331b90249772ce78d229
Sayan Samanta <samantasayan23@gmail.com>
2023-03-05 20:17:09 +0530

    added event

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const ROPSTEN_PRIVATE_KEY = "12a998ef40a80c25a5c16b06c10e254ef4774848b37857ed26b2a9ccb47ee967";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    Goerli: {
+      url: "https://goerli.infura.io/v3/a95462654a494a618f973f6cf7ae1b75",
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
+
 };
